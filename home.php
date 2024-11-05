114173393efbd7dd0f072484201e5ab986d25a92
Grayyy1219 <lance.musngi@gmail.com>
2024-11-05 23:42:16 +0800

    d

--- home.php
+++ home.php
@@ -1,15 +1,6 @@
 <?php
 session_start();
-$servername = "localhost"; 
-$username = "root"; 
-$password = ""; 
-$dbname = "vloginsight";
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
+include 'db.php';
 
 if (!isset($_SESSION['username'])) {
     header("Location: login.php"); 
